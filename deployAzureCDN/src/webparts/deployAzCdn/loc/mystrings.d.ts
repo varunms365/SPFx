2e4f97454cf37de1428c361c0c52f3f21b35929b
VarunMVA1 <50532358+VarunMVA1@users.noreply.github.com>
2020-01-09 09:32:33 -0500

    deploy Azure CDN
    deploy Azure CDN - SPFx 1.9.1

---
+++ deployAzureCDN/src/webparts/deployAzCdn/loc/mystrings.d.ts
@@ -0,0 +1,10 @@
+declare interface IDeployAzCdnWebPartStrings {
+  PropertyPaneDescription: string;
+  BasicGroupName: string;
+  DescriptionFieldLabel: string;
+}
+
+declare module 'DeployAzCdnWebPartStrings' {
+  const strings: IDeployAzCdnWebPartStrings;
+  export = strings;
+}
