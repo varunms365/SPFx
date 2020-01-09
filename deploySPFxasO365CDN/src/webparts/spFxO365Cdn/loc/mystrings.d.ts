0c76d7d91558d2472f2cb0175f6808285575b36d
VarunMVA1 <50532358+VarunMVA1@users.noreply.github.com>
2020-01-09 09:27:57 -0500

    deploy SPFx as O365 CDN
    deploy SPFx as O365 CDN - SPFx 1.9.1

---
+++ deploySPFxasO365CDN/src/webparts/spFxO365Cdn/loc/mystrings.d.ts
@@ -0,0 +1,10 @@
+declare interface ISpFxO365CdnWebPartStrings {
+  PropertyPaneDescription: string;
+  BasicGroupName: string;
+  DescriptionFieldLabel: string;
+}
+
+declare module 'SpFxO365CdnWebPartStrings' {
+  const strings: ISpFxO365CdnWebPartStrings;
+  export = strings;
+}
